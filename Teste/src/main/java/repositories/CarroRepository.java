985934bd9dc19fbea90686eb4c774223e1857164
CaioBuard <128753517+CaioBuard@users.noreply.github.com>
2024-05-16 21:44:37 -0300

    Add files via upload

---
+++ Teste/src/main/java/repositories/CarroRepository.java
@@ -0,0 +1,9 @@
+package repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import Models.Carro;
+
+public interface CarroRepository extends JpaRepository<Carro, Long>{
+
+}
